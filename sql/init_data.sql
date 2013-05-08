58da8e447d09628daf391719d54501edcf5938d5
waygaChan <553806198@qq.com>
2013-05-08 14:49:38 +0800

    添加数据库相关内容
    数据库模型～建表sql～初始化sql(menu表数据)

---
+++ sql/init_data.sql
@@ -0,0 +1,29 @@
+/*
+-- Query: SELECT * FROM QCS.menu
+LIMIT 0, 1000
+
+-- Date: 2013-05-08 14:47
+*/
+INSERT INTO `menu` (`id`,`parent_id`,`seq`,`title`,`uri`,`type`) VALUES (1,'0','11','教师管理','/teacherManager','2');
+INSERT INTO `menu` (`id`,`parent_id`,`seq`,`title`,`uri`,`type`) VALUES (2,'0','22','学生管理','/studentManager','2');
+INSERT INTO `menu` (`id`,`parent_id`,`seq`,`title`,`uri`,`type`) VALUES (3,'0','33','选题管理','/question','2');
+INSERT INTO `menu` (`id`,`parent_id`,`seq`,`title`,`uri`,`type`) VALUES (4,'0','44','名单管理','/namelist','2');
+INSERT INTO `menu` (`id`,`parent_id`,`seq`,`title`,`uri`,`type`) VALUES (5,'0','55','资料维护','/personal','2');
+INSERT INTO `menu` (`id`,`parent_id`,`seq`,`title`,`uri`,`type`) VALUES (6,'1','11','查询教师','/teacherManager/toQuery.do','2');
+INSERT INTO `menu` (`id`,`parent_id`,`seq`,`title`,`uri`,`type`) VALUES (7,'0','1','首页','/user/index.do','2');
+INSERT INTO `menu` (`id`,`parent_id`,`seq`,`title`,`uri`,`type`) VALUES (8,'5','1','密码修改','/personal/modifyPassword.do','2');
+INSERT INTO `menu` (`id`,`parent_id`,`seq`,`title`,`uri`,`type`) VALUES (9,'4','1','导入教师信息','/namelist/toImportTeacher.do','2');
+INSERT INTO `menu` (`id`,`parent_id`,`seq`,`title`,`uri`,`type`) VALUES (10,'4','11','导入学生信息','/namelist/importStudent.do','2');
+INSERT INTO `menu` (`id`,`parent_id`,`seq`,`title`,`uri`,`type`) VALUES (11,'0','11','题目管理','/question','0');
+INSERT INTO `menu` (`id`,`parent_id`,`seq`,`title`,`uri`,`type`) VALUES (12,'0','22','学生管理','/studentManager','0');
+INSERT INTO `menu` (`id`,`parent_id`,`seq`,`title`,`uri`,`type`) VALUES (13,'0','33','资料维护','/personal','0');
+INSERT INTO `menu` (`id`,`parent_id`,`seq`,`title`,`uri`,`type`) VALUES (14,'11','11','出题','/question/newQuestion.do','0');
+INSERT INTO `menu` (`id`,`parent_id`,`seq`,`title`,`uri`,`type`) VALUES (15,'12','11','查询学生','/studentManager/toQuery.do','0');
+INSERT INTO `menu` (`id`,`parent_id`,`seq`,`title`,`uri`,`type`) VALUES (16,'13','11','修改信息','/personal/modifyInfo.do','0');
+INSERT INTO `menu` (`id`,`parent_id`,`seq`,`title`,`uri`,`type`) VALUES (17,'0','1','首页','/user/index.do','0');
+INSERT INTO `menu` (`id`,`parent_id`,`seq`,`title`,`uri`,`type`) VALUES (18,'13','22','密码修改','/personal/modifyPassword.do','0');
+INSERT INTO `menu` (`id`,`parent_id`,`seq`,`title`,`uri`,`type`) VALUES (19,'0','11','首页','/user/index.do','1');
+INSERT INTO `menu` (`id`,`parent_id`,`seq`,`title`,`uri`,`type`) VALUES (20,'0','22','个人资料','/personal','1');
+INSERT INTO `menu` (`id`,`parent_id`,`seq`,`title`,`uri`,`type`) VALUES (21,'0','33','查看成绩排名','/score','1');
+INSERT INTO `menu` (`id`,`parent_id`,`seq`,`title`,`uri`,`type`) VALUES (22,'20','11','查看个人信息','/personal/personalInfo.do','1');
+INSERT INTO `menu` (`id`,`parent_id`,`seq`,`title`,`uri`,`type`) VALUES (23,NULL,'22','密码修改','/personal/modifyPassword.do','1');
---
+++ sql/qcs_create.sql
@@ -0,0 +1,178 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+DROP SCHEMA IF EXISTS `QCS` ;
+CREATE SCHEMA IF NOT EXISTS `QCS` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `QCS` ;
+
+-- -----------------------------------------------------
+-- Table `QCS`.`user_info`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `QCS`.`user_info` ;
+
+CREATE  TABLE IF NOT EXISTS `QCS`.`user_info` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `username` VARCHAR(45) NULL COMMENT '用户名' ,
+  `password` VARCHAR(32) NULL COMMENT '密码' ,
+  `state` VARCHAR(2) NULL COMMENT '状态\n0：正常\n1：冻结' ,
+  `type` VARCHAR(2) NULL COMMENT '类型\n0：教师\n1：学生\n2：管理员' ,
+  PRIMARY KEY (`id`) ,
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `QCS`.`teacher_info`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `QCS`.`teacher_info` ;
+
+CREATE  TABLE IF NOT EXISTS `QCS`.`teacher_info` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `user_id` INT NOT NULL ,
+  `teacher_no` VARCHAR(45) NULL COMMENT '教工号' ,
+  `email` VARCHAR(50) NULL COMMENT '邮箱' ,
+  `name` VARCHAR(45) NULL COMMENT '姓名' ,
+  `gender` VARCHAR(2) NULL COMMENT '性别\n0：女\n1：男' ,
+  `birth_date` VARCHAR(20) NULL COMMENT '出生日期' ,
+  `department` VARCHAR(50) NULL COMMENT '部门(院校)' ,
+  `education` VARCHAR(20) NULL COMMENT '学历' ,
+  `degree` VARCHAR(20) NULL COMMENT '学位' ,
+  `job` VARCHAR(50) NULL COMMENT '职务' ,
+  `job_title` VARCHAR(50) NULL COMMENT '职称' ,
+  `major` VARCHAR(50) NULL COMMENT '专业' ,
+  `graduated` VARCHAR(50) NULL COMMENT '毕业院校' ,
+  `teacher_cert` VARCHAR(2) NULL COMMENT '教师资格' ,
+  `remark` VARCHAR(50) NULL COMMENT '备注' ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_teacher_info_user_info1_idx` (`user_id` ASC) ,
+  UNIQUE INDEX `teacher_no_UNIQUE` (`teacher_no` ASC) ,
+  CONSTRAINT `fk_teacher_info_user_info1`
+    FOREIGN KEY (`user_id` )
+    REFERENCES `QCS`.`user_info` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `QCS`.`question`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `QCS`.`question` ;
+
+CREATE  TABLE IF NOT EXISTS `QCS`.`question` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `teacher_info_id` INT NOT NULL COMMENT '出题教师id' ,
+  `title` VARCHAR(100) NULL ,
+  `content` VARCHAR(500) NULL ,
+  `remark` VARCHAR(100) NULL ,
+  `state` VARCHAR(45) NULL COMMENT '状态\n0：审核中\n1：通过\n2：不通过' ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_question_teacher_info_idx` (`teacher_info_id` ASC) ,
+  CONSTRAINT `fk_question_teacher_info`
+    FOREIGN KEY (`teacher_info_id` )
+    REFERENCES `QCS`.`teacher_info` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `QCS`.`job`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `QCS`.`job` ;
+
+CREATE  TABLE IF NOT EXISTS `QCS`.`job` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `job` VARCHAR(45) NULL COMMENT '职务\n当与教室表的job字段匹配时候\n教师可以出题并且有数量限制' ,
+  `question_num` INT NULL COMMENT '出题数量' ,
+  PRIMARY KEY (`id`) ,
+  UNIQUE INDEX `job_UNIQUE` (`job` ASC) )
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `QCS`.`student_info`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `QCS`.`student_info` ;
+
+CREATE  TABLE IF NOT EXISTS `QCS`.`student_info` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `user_id` INT NOT NULL ,
+  `student_no` VARCHAR(45) NULL COMMENT '学号' ,
+  `email` VARCHAR(50) NULL COMMENT '邮箱' ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_student_info_user_info2_idx` (`user_id` ASC) ,
+  CONSTRAINT `fk_student_info_user_info2`
+    FOREIGN KEY (`user_id` )
+    REFERENCES `QCS`.`user_info` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `QCS`.`student_info`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `QCS`.`student_info` ;
+
+CREATE  TABLE IF NOT EXISTS `QCS`.`student_info` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `user_id` INT NOT NULL ,
+  `student_no` VARCHAR(45) NULL COMMENT '学号' ,
+  `email` VARCHAR(50) NULL COMMENT '邮箱' ,
+  PRIMARY KEY (`id`) ,
+  INDEX `fk_student_info_user_info2_idx` (`user_id` ASC) ,
+  CONSTRAINT `fk_student_info_user_info2`
+    FOREIGN KEY (`user_id` )
+    REFERENCES `QCS`.`user_info` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `QCS`.`student_question`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `QCS`.`student_question` ;
+
+CREATE  TABLE IF NOT EXISTS `QCS`.`student_question` (
+  `question_id` INT NOT NULL ,
+  `student_id` INT NOT NULL ,
+  INDEX `fk_student_question_question1_idx` (`question_id` ASC) ,
+  INDEX `fk_student_question_student_info1_idx` (`student_id` ASC) ,
+  PRIMARY KEY (`question_id`, `student_id`) ,
+  CONSTRAINT `fk_student_question_question1`
+    FOREIGN KEY (`question_id` )
+    REFERENCES `QCS`.`question` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_student_question_student_info1`
+    FOREIGN KEY (`student_id` )
+    REFERENCES `QCS`.`student_info` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `QCS`.`menu`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `QCS`.`menu` ;
+
+CREATE  TABLE IF NOT EXISTS `QCS`.`menu` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `parent_id` VARCHAR(5) NULL COMMENT '父菜单id' ,
+  `seq` VARCHAR(10) NULL COMMENT '排序序列' ,
+  `title` VARCHAR(45) NULL COMMENT '标题' ,
+  `uri` VARCHAR(100) NULL COMMENT 'uri\n' ,
+  `type` VARCHAR(2) NULL COMMENT '类型\n0：教师\n1：学生\n2：管理员' ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB;
+
+USE `QCS` ;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
